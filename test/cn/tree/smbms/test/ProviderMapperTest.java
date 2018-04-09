3ad957e214fedcde98e2ca81cdc349dcecb45bff
wangbin12123 <522392008@qq.com>
2018-04-09 09:52:17 +0800

    增加供应商的查询接口

--- test/cn/tree/smbms/test/ProviderMapperTest.java
+++ test/cn/tree/smbms/test/ProviderMapperTest.java
@@ -2,19 +2,26 @@ package cn.tree.smbms.test;
 
 import java.io.IOException;
 import java.io.InputStream;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.log4j.Logger;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import cn.three.smbms.pojo.provider;
+import cn.tree.smbms.dao.provider.ProviderMapper;
+import cn.tree.smbms.utils.MyBatisUtil;
 
 
 
 public class ProviderMapperTest {
 	private Logger logger = Logger.getLogger(ProviderMapperTest.class);
+	@Ignore
 	@Test
 	public void TestCount() {
 		String resource = "mybatis-config.xml";
@@ -34,6 +41,23 @@ public class ProviderMapperTest {
 		}finally {
 			//¹Ø±Õsession
 			session.close();
+		}
+	}
+	@Test
+	public void TestProName() {
+		List<provider> list = new ArrayList<provider>();
+		SqlSession session = null;
+		try {
+			session = MyBatisUtil.createSession();
+			//list = session.selectList("cn.tree.smbms.dao.provider.ProviderMapper.getProviderList");
+			list = session.getMapper(ProviderMapper.class).getProviderList();
+			for (provider provider : list) {
+				logger.debug("proName\t"+provider.getProName());
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			MyBatisUtil.closeSession(session);
 		}
 	}
 }
